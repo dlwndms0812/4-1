502df76d58bda6f07628ec8a6aa9f78750846699
dlwndms0812 <dlwndms0812@naver.com>
2023-03-24 21:10:33 +0900

    실습 완료

--- week3/Untitled.py
+++ week3/Untitled.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
+# In[1]:
 
 
 import os
@@ -22,13 +22,13 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path = HOUSING_PATH):
   housing_tgz.close()
 
 
-# In[25]:
+# In[2]:
 
 
 fetch_housing_data()
 
 
-# In[26]:
+# In[4]:
 
 
 import pandas as pd
@@ -38,41 +38,41 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
-# In[27]:
+# In[5]:
 
 
 housing = load_housing_data()
 housing.head()
 
 
-# In[28]:
+# In[6]:
 
 
 housing.info()
 
 
-# In[29]:
+# In[7]:
 
 
 housing["ocean_proximity"].value_counts()
 
 
-# In[30]:
+# In[8]:
 
 
 housing.describe()
 
 
-# In[31]:
+# In[9]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
-housing.hist(bins=50, figsize=(20, 15))
+housing.hist(bins=50, figsize=(20,15))
 plt.show()
 
 
-# In[32]:
+# In[11]:
 
 
 from sklearn.model_selection import train_test_split
@@ -81,33 +81,33 @@ import numpy as np
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 
-# In[33]:
+# In[12]:
 
 
 test_set.head()
 
 
-# In[35]:
+# In[13]:
 
 
-housing["income_cat"] = pd.cut(housing["median_income"],   
+housing["income_cat"] = pd.cut(housing["median_income"],
                               bins=[0, 1.5, 3.0, 4.5, 6, np.inf],
-                              labels=[1,2,3,4,5])
+                              labels=[1, 2, 3, 4, 5])
 
 
-# In[36]:
+# In[14]:
 
 
 housing["income_cat"].hist()
 
 
-# In[37]:
+# In[15]:
 
 
 housing["income_cat"].value_counts()
 
 
-# In[42]:
+# In[16]:
 
 
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -118,21 +118,201 @@ for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_test_set = housing.loc[test_index]
 
 
-# In[43]:
+# In[18]:
 
 
 strat_test_set["income_cat"].value_counts() / len(strat_test_set)
 
 
-# In[44]:
+# In[19]:
 
 
-for set_ in(strat_train_set, strat_test_set):
+for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 
 
-# In[ ]:
+# In[23]:
 
 
+housing = strat_train_set.copy()
 
+
+# In[25]:
+
+
+housing.plot(kind="scatter", x="longitude", y="latitude")
+
+
+# In[26]:
+
+
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+
+
+# In[28]:
+
+
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+            s=housing["population"]/100, label="population", figsize=(10, 7),
+            c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+            sharex=False)
+plt.legend()
+
+
+# In[29]:
+
+
+corr_matrix = housing.corr()
+
+
+# In[30]:
+
+
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+
+# In[33]:
+
+
+from pandas.plotting import scatter_matrix
+
+attributes = ["median_house_value", "median_income", "total_rooms",
+             "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12, 8))
+
+
+# In[34]:
+
+
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"] = housing["population"]/housing["households"]
+
+
+# In[35]:
+
+
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+
+# In[36]:
+
+
+sample_incomplete_rows = housing[housing.isnull().any(axis=1)].head()
+sample_incomplete_rows
+
+
+# In[37]:
+
+
+sample_incomplete_rows.dropna(subset=["total_bedrooms"])
+
+
+# In[38]:
+
+
+sample_incomplete_rows.drop("total_bedrooms", axis=1)
+
+
+# In[39]:
+
+
+median = housing["total_bedrooms"].median()
+sample_incomplete_rows["total_bedrooms"].fillna(median, inplace=True)
+
+
+# In[40]:
+
+
+sample_incomplete_rows
+
+
+# In[41]:
+
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+
+
+# In[42]:
+
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+
+
+# In[43]:
+
+
+imputer.fit(housing_num)
+
+
+# In[44]:
+
+
+imputer.statistics_
+
+
+# In[45]:
+
+
+housing_num.median().values
+
+
+# In[46]:
+
+
+X = imputer.transform(housing_num)
+
+
+# In[47]:
+
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                         index=housing_num.index)
+
+
+# In[49]:
+
+
+housing_cat = housing[["ocean_proximity"]]
+housing_cat.head(10)
+
+
+# In[50]:
+
+
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+housing_cat_encoded[:10]
+
+
+# In[52]:
+
+
+ordinal_encoder.categories_
+
+
+# In[53]:
+
+
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder = OneHotEncoder()
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+housing_cat_1hot
+
+
+# In[54]:
+
+
+housing_cat_1hot.toarray()
+
+
+# In[55]:
+
+
+cat_encoder.categories_
 
